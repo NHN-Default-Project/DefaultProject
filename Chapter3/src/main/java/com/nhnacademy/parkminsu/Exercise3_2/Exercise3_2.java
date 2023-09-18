5225f60248d743957c9ddfba70b10d7fb25d20da
minsu <zmfjtnl1@naver.com>
2023-09-18 12:27:37 +0900

    chapter3 file upload and chapter9 new create

---
+++ Chapter3/src/main/java/com/nhnacademy/parkminsu/Exercise3_2/Exercise3_2.java
@@ -0,0 +1,16 @@
+package com.nhnacademy.parkminsu.Exercise3_2;
+
+import com.nhnacademy.parkminsu.Exercise3_6.DivisiorCollection;
+
+public class Exercise3_2 {
+    public static void main(String[] args) {
+        DivisiorCollection divisiorCollection = new DivisiorCollection(1, 10000);
+        int num = 1;
+        if (num != 1) {
+            System.exit(2);
+        }
+
+        divisiorCollection.execution(num);
+    }
+
+}
