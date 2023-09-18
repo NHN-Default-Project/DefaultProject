871804975436564b3677be87ad61503a370c3267
hyeonjaez <jhjh0022@naver.com>
2023-09-18 20:34:58 +0900

    Problem3 modify

--- Chapter4/src/main/java/com/nhnacademy/jaehyeon/Problem3.java
+++ Chapter4/src/main/java/com/nhnacademy/jaehyeon/Problem3.java
@@ -1,5 +1,7 @@
 package com.nhnacademy.jaehyeon;
 
+import java.util.Scanner;
+
 public class Problem3 {
 
     public static int diceNumber() {
@@ -7,23 +9,26 @@ public class Problem3 {
     }
 
     public static void main(String[] args) {
-        playDiceGame();
+        Scanner scanner = new Scanner(System.in);
+        int inputNumber = scanner.nextInt();
+        verifySumRange(inputNumber);
+
+        playDiceGame(inputNumber);
     }
 
 
-    public static void playDiceGame() {
+    public static void playDiceGame(int inNum) {
         int count = 0;
         while (true) {
             count++;
             int sum = diceNumber() + diceNumber();
-            verifySumRange(sum);
-
-            if (sum == 2) {
+            if (sum == inNum) {
                 break;
             }
         }
-        System.out.println("뱀눈이 나올때까지 걸린 횟수 : " + count);
+        System.out.printf("입력한 값 %d가 나올때까지 걸린 횟수 : %d회", inNum, count);
     }
+
 
     public static void verifySumRange(int sum) {
         if (2 > sum || sum > 12) {
