ff90a92be3990820a2979cd7568fd9addcb9251e
hyeonjaez <jhjh0022@naver.com>
2023-09-19 16:48:18 +0900

    modify : Chapter4_Problem2

--- Chapter4/src/main/java/com/nhnacademy/jaehyeon/Problem2.java
+++ Chapter4/src/main/java/com/nhnacademy/jaehyeon/Problem2.java
@@ -6,21 +6,13 @@ public class Problem2 {
 
 
     public static void main(String[] args) {
-        String str = "34A7";
-        Scanner scaner = new Scanner(System.in);
-        String inputString = scaner.next();
-        verifyInputData(inputString);
+
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.next();
         System.out.println(hexValue(inputString));
 
-    }
+        scanner.close();
 
-    public static void verifyInputData(String str) {
-        for (int i = 0; i < str.length(); i++) {
-            if (!(('0' <= str.charAt(i) && str.charAt(i) <= '9')
-                    || 'A' <= str.charAt(i) && str.charAt(i) <= 'F')) {
-                throw new IllegalArgumentException("16진수의 숫자들만 넣어주세요");
-            }
-        }
     }
 
     public static int hexValue(String str) {
@@ -28,35 +20,89 @@ public class Problem2 {
         for (int i = 0; i < str.length(); i++) {
             int num = 0;
 
-
             if (('0' <= str.charAt(i) && str.charAt(i) <= '9')) {
                 num = str.charAt(i) - '0';
-                System.out.println(num);
+                switch (num) {
+                    case 0:
+                        num = 0;
+                        break;
+
+                    case 1:
+                        num = 1;
+                        break;
+
+                    case 2:
+                        num = 2;
+                        break;
+
+                    case 3:
+                        num = 3;
+                        break;
+
+                    case 4:
+                        num = 4;
+                        break;
+
+                    case 5:
+                        num = 5;
+                        break;
+
+                    case 6:
+                        num = 6;
+                        break;
+
+                    case 7:
+                        num = 7;
+                        break;
+
+                    case 8:
+                        num = 8;
+                        break;
+
+                    case 9:
+                        num = 9;
+                        break;
+
+                }
+
             } else {
                 switch (str.charAt(i)) {
                     case 'A':
+                    case 'a':
                         num = 10;
                         break;
                     case 'B':
+                    case 'b':
                         num = 11;
                         break;
                     case 'C':
+                    case 'c':
                         num = 12;
                         break;
                     case 'D':
+                    case 'd':
                         num = 13;
                         break;
                     case 'E':
+                    case 'e':
                         num = 14;
                         break;
                     case 'F':
+                    case 'f':
                         num = 15;
+                        break;
+                    default:
+                        num = -1;
                         break;
                 }
 
             }
 
-            value += calculateDigit(str.length() - 1 - i, num);
+            if (num == -1) {
+                return -1;
+            }
+
+            value += (int) calculateDigit(str.length() - 1 - i, num);
         }
         return value;
     }
