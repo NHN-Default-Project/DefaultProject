1dda74827aaaa4db7fd362e533892a87d3b54618
minsu <zmfjtnl1@naver.com>
2023-09-20 18:42:15 +0900

    Modified Chapter5 Exercise5_3Package Exercise5_3.java

--- Chapter5/src/main/java/com/nhnacademy/parkminsu/Exercise5_3/Exercise5_3.java
+++ Chapter5/src/main/java/com/nhnacademy/parkminsu/Exercise5_3/Exercise5_3.java
@@ -5,24 +5,24 @@ import com.nhnacademy.parkminsu.Exercise5_2.StatCalc;
 
 public class Exercise5_3 {
     private static final int ARANGE = 10000;
-    private static final int BEGIN_DiceSum_Range = 2;
-    private static final int END_DiceSum_Range = 12;
+    private static final int BEGIN_DICESUM_RANGE = 2;
+    private static final int END_DICESUM_RANGE = 12;
 
-    private static final int ARRAYSize = END_DiceSum_Range - BEGIN_DiceSum_Range + 1;
+    private static final int ARRAYSIZE = END_DICESUM_RANGE - BEGIN_DICESUM_RANGE + 1;
 
     public static void main(String[] args) {
 
-        StatCalc[] statCalc = new StatCalc[ARRAYSize];
-        PairOfDice[] pairOfDice = new PairOfDice[ARRAYSize];
+        StatCalc[] statCalc = new StatCalc[ARRAYSIZE];
+        PairOfDice[] pairOfDice = new PairOfDice[ARRAYSIZE];
 
 
-        for (int i = 0; i < ARRAYSize; i++) {
+        for (int i = 0; i < ARRAYSIZE; i++) {
             statCalc[i] = new StatCalc();
-            pairOfDice[i] = new PairOfDice(BEGIN_DiceSum_Range + i);
+            pairOfDice[i] = new PairOfDice(BEGIN_DICESUM_RANGE + i);
         }
         statCalc = createStatCalcValue(statCalc, pairOfDice);
-        for (int i = 0; i < ARRAYSize; i++) {
-            System.out.println(BEGIN_DiceSum_Range + i + "의 통계");
+        for (int i = 0; i < ARRAYSIZE; i++) {
+            System.out.println(BEGIN_DICESUM_RANGE + i + "의 통계");
             System.out.println("평균:\t" + statCalc[i].getMean());
             System.out.println("표준편차:\t" + statCalc[i].getStandardDeviation());
             System.out.println("최대값:\t" + statCalc[i].getMax());
@@ -34,18 +34,17 @@ public class Exercise5_3 {
 
 
     public static StatCalc[] createStatCalcValue(StatCalc[] statCalcsList, PairOfDice[] pairOfDice) {
-        for (int i = BEGIN_DiceSum_Range; i <= END_DiceSum_Range; i++) {
+        for (int i = BEGIN_DICESUM_RANGE; i <= END_DICESUM_RANGE; i++) {
             int count = 0;
             while (true) {
                 if (count == ARANGE) {
-                    System.out.println("break");
                     break;
                 }
-                pairOfDice[i - BEGIN_DiceSum_Range].roll();
-                if (pairOfDice[i - BEGIN_DiceSum_Range].sumDice() == i) {
-                    statCalcsList[i - BEGIN_DiceSum_Range].enter(pairOfDice[i - BEGIN_DiceSum_Range].getRollDiceCount());
+                pairOfDice[i - BEGIN_DICESUM_RANGE].roll();
+                if (pairOfDice[i - BEGIN_DICESUM_RANGE].sumDice() == i) {
+                    statCalcsList[i - BEGIN_DICESUM_RANGE].enter(pairOfDice[i - BEGIN_DICESUM_RANGE].getRollDiceCount());
                     count++;
-                    pairOfDice[i - BEGIN_DiceSum_Range].initRollDiceCount();
+                    pairOfDice[i - BEGIN_DICESUM_RANGE].initRollDiceCount();
                 }
             }
         }
