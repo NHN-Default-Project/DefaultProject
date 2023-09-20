f2f5f30abda4b714dd12cb467fe97b107386529a
minsu <zmfjtnl1@naver.com>
2023-09-20 19:43:38 +0900

    Modified Chapter4 Exercise4_1.java

--- Chapter4/src/main/java/com/nhnacademy/parkminsu/Exercise4_1/Exercise4_1.java
+++ Chapter4/src/main/java/com/nhnacademy/parkminsu/Exercise4_1/Exercise4_1.java
@@ -5,13 +5,12 @@ import java.util.Scanner;
 public class Exercise4_1 {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-            Exercise4_1 main = new Exercise4_1();
-            System.out.println(main.printCapialized(scanner.nextLine()));
+            System.out.println(printCapialized(scanner.nextLine()));
         }
 
     }
 
-    public StringBuilder printCapialized(String str) {
+    public static StringBuilder printCapialized(String str) {
         StringBuilder resultString = new StringBuilder();
         boolean upperCheck = false;
         for (int idx = 0; idx < str.length(); idx++) {
