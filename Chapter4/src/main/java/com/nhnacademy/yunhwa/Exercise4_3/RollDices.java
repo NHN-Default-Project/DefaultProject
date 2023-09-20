acc84405dfa16a3fa37b0d1d404b79f3415ab1be
{prospro1} <{cldufgkrp2023@gmail.com}>
2023-09-20 13:20:42 +0900

    yunhwa modified the Exercise4_3/RollDices -> rollManyDice() 메서드  안의 dices.add()하는 for문을 생성자로 옮겨줌

--- Chapter4/src/main/java/com/nhnacademy/yunhwa/Exercise4_3/RollDices.java
+++ Chapter4/src/main/java/com/nhnacademy/yunhwa/Exercise4_3/RollDices.java
@@ -6,8 +6,12 @@ import java.util.List;
 public class RollDices { // 주사위를 돌리자
     private List<Dice> dices; // 돌릴 주사위들이 필요해
 
-    public RollDices(int dicesCount) { // 주사위 개수만큼 돌릴래
+    public RollDices(int dicesCount, int totalEyesNum) { // 주사위 개수만큼 돌릴래
         dices = new ArrayList<Dice>(dicesCount);
+
+        for (int i = 0; i < dicesCount; i++) {
+            dices.add(new Dice(totalEyesNum));
+        }
     }
 
     public List<Dice> getDices() { // 주사위들의 정보를 얻어오자
@@ -20,10 +24,6 @@ public class RollDices { // 주사위를 돌리자
 
     // 주사위들 돌리는 작업 메서드 (마치 setter 같음): 무조건 여기는 거쳐야 함. 돌릴 주사위를 한 번 돌려야지 현재 눈이 결정되기 때문.
     public void rollManyDice(int dicesCount, int totalEyesNum) { // totalEyesNum 면체 주사위 여러 개를 돌리자
-
-        for (int i = 0; i < dicesCount; i++) {
-            dices.add(new Dice(totalEyesNum));
-        }
 
         for (int i = 0; i < dices.size(); i++) {
             dices.get(i).rollDice(); // 각각의 현재 눈이 결정됨.
