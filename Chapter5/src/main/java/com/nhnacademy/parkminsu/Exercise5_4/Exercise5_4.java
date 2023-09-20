912091209c24c351d45290526e23490f7c81958a
minsu <zmfjtnl1@naver.com>
2023-09-20 12:50:15 +0900

    Modified Chapter5 Exercise5_4Package Exercise5_4.java

--- Chapter5/src/main/java/com/nhnacademy/parkminsu/Exercise5_4/Exercise5_4.java
+++ Chapter5/src/main/java/com/nhnacademy/parkminsu/Exercise5_4/Exercise5_4.java
@@ -7,17 +7,27 @@ public class Exercise5_4 {
     private static final int MAX_CARD_COUNT = 6;
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        BlackjackHand blackjackHand = new BlackjackHand();
-        Deck deck = new Deck(false);
-        int num = scanner.nextInt();
-        if (num < MIN_CARD_COUNT && num > MAX_CARD_COUNT) {
-            throw new IllegalArgumentException("카드의 갯수 범위가 잘못되었습니다.");
+
+        try (Scanner scanner = new Scanner(System.in);) {
+            BlackjackHand blackjackHand = new BlackjackHand();
+            Deck deck = new Deck(false);
+            System.out.print("카드 몇장을 받을건가요? ");
+            int num = scanner.nextInt();
+
+            if (num < MIN_CARD_COUNT || num > MAX_CARD_COUNT) {
+                throw new IllegalArgumentException("카드의 갯수 범위가 잘못되었습니다.");
+            }
+            deck.shuffle();
+            for (int i = 0; i < num; i++) {
+                blackjackHand.addCard(deck.dealCard());
+                System.out.println(blackjackHand.getCard(i));
+            }
+
+            System.out.println(blackjackHand.getBlackjackValue());
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         }
-        deck.shuffle();
-        for (int i = 0; i < num; i++) {
-            blackjackHand.addCard(deck.dealCard());
-        }
-        System.out.println(blackjackHand.getBlackjackValue());
+
     }
 }
