770900e937b99cc75de78c52a19b2d7147ac7fc3
minsu <zmfjtnl1@naver.com>
2023-09-20 13:15:52 +0900

    Modified Chapter5_7Package Exercise5_7.java

--- Chapter5/src/main/java/com/nhnacademy/parkminsu/Exercise5_7/Exercise5_7.java
+++ Chapter5/src/main/java/com/nhnacademy/parkminsu/Exercise5_7/Exercise5_7.java
@@ -5,11 +5,11 @@ import com.nhnacademy.parkminsu.Exercise5_6.AdditionQuestion;
 import java.util.Scanner;
 
 public class Exercise5_7 {
-    private static final int range = 10;
+    private static final int RANGE = 10;
 
     public static void main(String[] args) {
-        IntQuestion[] substract = new IntQuestion[range];
-        int[] answer = new int[range];
+        IntQuestion[] substract = new IntQuestion[RANGE];
+        int[] answer = new int[RANGE];
         substract = createArrayIntQuestion(substract);
         answer = writeAnswer(substract, answer);
         checkAnswerList(substract, answer);
@@ -18,7 +18,7 @@ public class Exercise5_7 {
 
 
     public static IntQuestion[] createArrayIntQuestion(IntQuestion[] intQuestions) {
-        for (int i = 0; i < range; i++) {
+        for (int i = 0; i < RANGE; i++) {
             if (i % 2 == 0) {
                 intQuestions[i] = new SubstractQuestion();
             } else {
@@ -30,7 +30,7 @@ public class Exercise5_7 {
 
     public static int[] writeAnswer(IntQuestion[] intQuestions, int[] answer) {
         Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < range; i++) {
+        for (int i = 0; i < RANGE; i++) {
             if (i % 2 == 0) {
                 System.out.println(intQuestions[i].getQuestion());
             } else {
@@ -39,11 +39,12 @@ public class Exercise5_7 {
 
             answer[i] = scanner.nextInt();
         }
+        scanner.close();
         return answer;
     }
 
     public static void checkAnswerList(IntQuestion[] intQuestions, int[] answer) {
-        for (int i = 0; i < range; i++) {
+        for (int i = 0; i < RANGE; i++) {
             if (checkAnswer(intQuestions[i].getCorrectAnswer(), answer[i])) {
                 System.out.println(i + 1 + "번은 정답입니다.");
             } else {
