21ebf4a430eac8863a2ad9d3368674d01d2b5841
minsu <zmfjtnl1@naver.com>
2023-09-21 14:59:35 +0900

    FEAT Chapter4 Exercise4_1

--- Chapter4/src/main/java/com/nhnacademy/parkminsu/Exercise4_1/Exercise4_1.java
+++ Chapter4/src/main/java/com/nhnacademy/parkminsu/Exercise4_1/Exercise4_1.java
@@ -12,22 +12,25 @@ public class Exercise4_1 {
 
     public static StringBuilder printCapialized(String str) {
         StringBuilder resultString = new StringBuilder();
-        boolean upperCheck = false;
+        int count = 0;
         for (int idx = 0; idx < str.length(); idx++) {
             if (Character.isLetter(str.charAt(idx))) {
-                if (!upperCheck) {
-                    upperCheck = true;
+                if (checkUpper(str.charAt(idx), count)) {
                     resultString.append(Character.toUpperCase(str.charAt(idx)));
+                    count++;
                 } else {
                     resultString.append(str.charAt(idx));
                 }
             } else {
+                count = 0;
                 resultString.append(str.charAt(idx));
-                upperCheck = false;
             }
 
         }
         return resultString;
     }
 
+    public static boolean checkUpper(char letter, int count) {
+        return ('a' <= letter || letter <= 'z') && count == 0;
+    }
 }
