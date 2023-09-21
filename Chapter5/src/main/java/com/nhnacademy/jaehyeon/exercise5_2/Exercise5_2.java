14165a85d87442ed988c248672d8f1964dd6fc41
hyeonjaez <jhjh0022@naver.com>
2023-09-21 12:47:05 +0900

    FEAT(Chapter5_Exercise5_2) : 숫자 아닌 문자가 입력되었을때 다시 질문 하는 기능 구현

--- Chapter5/src/main/java/com/nhnacademy/jaehyeon/exercise5_2/Exercise5_2.java
+++ Chapter5/src/main/java/com/nhnacademy/jaehyeon/exercise5_2/Exercise5_2.java
@@ -5,21 +5,43 @@ import java.util.Scanner;
 public class Exercise5_2 {
 
     public static void main(String[] args) {
-        StatCalc statCalc = new StatCalc();
-        Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            System.out.println("숫자를 입력하세요");
-            System.out.println("0을 입력하면 입력을 멈춘다는것 입니다.");
-            int num = scanner.nextInt();
-            if (num == 0) {
-                break;
-            }
-            statCalc.enter(num);
-        }
+        StatCalc statCalc = new StatCalc();
+        inputNumber(statCalc);
         System.out.println(statCalc.toString());
 
-        scanner.close();
+
+    }
+
+    public static void inputNumber(StatCalc statCalc) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("숫자를 입력하세요");
+        System.out.println("0을 입력하면 입력을 멈춘다는것 입니다.");
+        String num = scanner.nextLine();
+
+        if (!isDoubleNumber(num)) {
+            System.out.println("숫자만 입력해주세요");
+            inputNumber(statCalc);
+        } else if (Double.parseDouble(num) == 0) {
+            scanner.close();
+            return;
+        } else {
+            statCalc.enter(Double.parseDouble(num));
+            inputNumber(statCalc);
+
+        }
+
+
+    }
+
+    private static boolean isDoubleNumber(String number) {
+        try {
+            Double.parseDouble(number);
+            return true;
+        } catch (NumberFormatException num) {
+            return false;
+        }
     }
 }
 
