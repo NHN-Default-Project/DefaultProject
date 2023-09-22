9620cf2e574820e0d2be278b2ff8491dd03de6bf
nannda3463 <minsu3463@gmail.com>
2023-09-22 11:16:43 +0900

    FEAT(Chapter7_3, Chapter7_5) - Excercise 구현

--- Chapter7/src/main/java/com/nhnacademy/Jminsoo/excercise3/Excercise7_3.java
+++ Chapter7/src/main/java/com/nhnacademy/Jminsoo/excercise3/Excercise7_3.java
@@ -1,8 +1,36 @@
 package com.nhnacademy.Jminsoo.excercise3;
 
+import java.util.Arrays;
+
+
 public class Excercise7_3 {
+    static final int LENGTH = 10_000;
+
+    static final int RANGE = 10_000;
+
+    static final double PERCENTAGE = 1_000_000_000.0;
+
     public static void main(String[] args) {
-        String[] strArray = makeArray(100000, 100);
+        String[] firstStrArray = makeArray(LENGTH, RANGE);
+        String[] secondStrArray = makeArray(LENGTH, RANGE);
+
+        long firstStartTime = System.nanoTime();
+
+        arraySort(firstStrArray);
+
+        long firstEndTime = System.nanoTime();
+
+        System.out.println("Array.sort 연산 시간 :  " + (firstEndTime - firstStartTime) / PERCENTAGE);
+
+        long secondStartTime = System.nanoTime();
+
+        selectionSort(secondStrArray);
+
+        long secondEndTime = System.nanoTime();
+
+        System.out.println("Selection Sort 연산 시간 :  " + (secondEndTime - secondStartTime) / PERCENTAGE);
+
+
     }
 
     public static String[] makeArray(int length, int range) {
@@ -14,5 +42,31 @@ public class Excercise7_3 {
 
         return resultArray;
     }
+
+    public static void arraySort(String[] strArray) {
+        Arrays.sort(strArray);
+    }
+
+    private static void selectionSort(String[] strArray) {
+
+        for (int lastPlace = strArray.length - 1; lastPlace > 0; lastPlace--) {
+
+            int maxLoc = 0;
+
+            for (int j = 1; j <= lastPlace; j++) {
+                if (Double.parseDouble(strArray[j]) > Double.parseDouble(strArray[maxLoc])) {
+
+                    maxLoc = j;
+                }
+            }
+
+            String temp = strArray[maxLoc];
+            strArray[maxLoc] = strArray[lastPlace];
+            strArray[lastPlace] = temp;
+
+        }  // end of for loop
+
+    }
+
 }
 
