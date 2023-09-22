f90600e6fb86a53b86b27eff6344d9b286e34487
20203174 <kaeun0000@naver.com>
2023-09-22 15:39:34 +0900

    FEAT: Chapter7

---
+++ Chapter7/src/main/java/com/nhnacademy/gaeun/exercise1/Exercise7_1.java
@@ -0,0 +1,10 @@
+package com.nhnacademy.gaeun.exercise1;
+
+/*
+* https://math.hws.edu/javanotes/c7/exercises.html
+* exercise7_1
+* 서브루틴 작성: 1~지정된 최대값까지의 임의의 정수를 포함하는 ArrayList생성
+* 매개변수는 입력받을 정수의 수와 지정된 최댓값
+*/
+public class Exercise7_1 {
+}
