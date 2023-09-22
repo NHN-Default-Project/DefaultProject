bc1dff41f5d89478001a8e6e94d91770978fdd94
minsu <zmfjtnl1@naver.com>
2023-09-22 11:56:57 +0900

    Jminsoo mistake merge

---
+++ Chapter3/src/main/java/com/nhnacademy/gaeun/exercise03/Main.java
@@ -0,0 +1,37 @@
+
+package com.nhnacademy.gaeun.exercise03;
+
+import com.nhnacademy.gaeun.textio.TextIO;
+
+public class Main {
+    char operation;
+    double first, second;
+
+    public static void main(String[] args) {
+        double large = TextIO.getDouble();
+        char operation = TextIO.getChar();
+        double small = TextIO.getDouble();
+
+        System.out.println(calculate(large, operation, small));
+
+    }
+
+    public static double calculate(double large, char operation, double small) {
+        double result = 0;
+        switch (operation) {
+            case '+':
+                result = large + small;
+                break;
+            case '-':
+                result = large - small;
+                break;
+            case '*':
+                result = large * small;
+                break;
+            case '/':
+                result = large / small;
+                break;
+        }
+        return result;
+    }
+}
---
+++ Chapter3/src/main/java/com/nhnacademy/parkminsu/Exercise3_6/Exercise3_6.java
@@ -0,0 +1,10 @@
+package com.nhnacademy.parkminsu.Exercise3_6;
+
+public class Exercise3_6 {
+    public static void main(String[] args) {
+        DivisiorCollection number = new DivisiorCollection(1, 10000);
+        number.execution(2);
+
+    }
+
+}
