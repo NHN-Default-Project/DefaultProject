b5845070c6fbfa7ba37b3d4025662d04fddffbc7
20203174 <kaeun0000@naver.com>
2023-09-23 18:09:20 +0900

    Assignment: Dictionary1

---
+++ Assignment/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        CsvDictionary csvDictionary = new CsvDictionary();
+        csvDictionary.load("Assignment/src/test/resources/test2.csv");
+
+        System.out.println("searching word: " + csvDictionary.findEngByKor("한국"));
+        System.out.println("total key size: " + csvDictionary.count());
+        System.out.println("key Set is: " + csvDictionary.findAllListKor());
+    }
+}
