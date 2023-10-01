00c252a4f767445bd3c730774e7d44bc569b549a
hyeonjaez <jhjh0022@naver.com>
2023-10-02 03:11:00 +0900

    REFACTOR: repeat method -> addUnit() combine

--- Assignment/src/main/java/org/example/jaehyeon/tribe/Tribe.java
+++ Assignment/src/main/java/org/example/jaehyeon/tribe/Tribe.java
@@ -1,6 +1,7 @@
 package org.example.jaehyeon.tribe;
 
 import org.example.jaehyeon.starcraft.Unit;
+
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import java.util.Random;
@@ -8,6 +9,12 @@ import java.util.Random;
 public abstract class Tribe {
 
     List<Unit> unitList;
+
+    public void addUnit(int unitNumber, String[] unit) {
+        for (int i = 0; i < unitNumber; i++) {
+            unitList.add(randomProductionUnit(unit));
+        }
+    }
 
     public Unit randomProductionUnit(String[] unit) {
         Random random = new Random();
@@ -21,6 +28,7 @@ public abstract class Tribe {
         }
 
     }
+
 
     public List<Unit> getUnitList() {
         return unitList;
