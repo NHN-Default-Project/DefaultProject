395f94d6b8ef87dbf25b6bad29b4221cd0761065
hyeonjaez <jhjh0022@naver.com>
2023-10-02 02:04:39 +0900

    FEAT : Wraith class

--- Assignment/src/main/java/org/example/jaehyeon/starcraft/terran/Wraith.java
+++ Assignment/src/main/java/org/example/jaehyeon/starcraft/terran/Wraith.java
@@ -1,4 +1,13 @@
-package starcraft.terran;
+package org.example.jaehyeon.starcraft.terran;
 
-public class Wraith {
+import org.example.jaehyeon.starcraft.Unit;
+
+public class Wraith extends Unit {
+
+    private static final int EXTRA_ATTACK_POWER = 3;
+    private static final int EXTRA_DEFENSE_POWER = 10;
+
+    public Wraith() {
+        setAbility(EXTRA_ATTACK_POWER, EXTRA_DEFENSE_POWER);
+    }
 }
