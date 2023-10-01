11cb89ea32434d676ccd61d0c2703aa6495677f7
hyeonjaez <jhjh0022@naver.com>
2023-10-02 03:02:51 +0900

    FEAT : Guardian class

---
+++ Assignment/src/main/java/org/example/jaehyeon/starcraft/zerg/Guardian.java
@@ -0,0 +1,13 @@
+package org.example.jaehyeon.starcraft.zerg;
+
+import org.example.jaehyeon.starcraft.Unit;
+
+public class Guardian extends Unit {
+
+    private static final int EXTRA_ATTACK_POWER = 4;
+    private static final int EXTRA_DEFENSE_POWER = 12;
+
+    public Guardian() {
+        setAbility(EXTRA_ATTACK_POWER, EXTRA_DEFENSE_POWER);
+    }
+}
