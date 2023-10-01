91be04c140d9854557ddaab19a98f5d780440d74
hyeonjaez <jhjh0022@naver.com>
2023-10-02 02:34:29 +0900

    FIX : constructor -> unitList 초기화

--- Assignment/src/main/java/org/example/jaehyeon/tribe/Terran.java
+++ Assignment/src/main/java/org/example/jaehyeon/tribe/Terran.java
@@ -1,36 +1,19 @@
 package org.example.jaehyeon.tribe;
 
-
 import org.example.jaehyeon.starcraft.terran.*;
+import java.util.ArrayList;
 
 public class Terran extends Tribe {
 
     private static final String[] UNIT = {"marine", "tank", "goliath", "wraith", "valkyrie"};
-    int PRODUCTION_NUMBER = 5;
+    private static final int PRODUCTION_NUMBER = 5;
 
     public Terran() {
+        unitList = new ArrayList<>();
         for (int i = 0; i < PRODUCTION_NUMBER; i++) {
             unitList.add(randomProductionUnit(UNIT));
         }
     }
-
-//    public Unit randomProductionUnit() {
-//        Random random = new Random();
-//        try {
-//            String selectUnit = UNIT[random.nextInt(UNIT.length)];
-//
-//            return (Unit) this.getClass().getMethod(selectUnit).invoke(selectUnit);
-//
-//        } catch (NoSuchMethodException e) {
-//            throw new RuntimeException(e);
-//        } catch (InvocationTargetException e) {
-//            throw new RuntimeException(e);
-//        } catch (IllegalAccessException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//    }
-
 
     public Marine marine() {
         return new Marine();
