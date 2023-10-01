bbd91f2d8978d258c0f532c77d40fd6af7bb2c1e
hyeonjaez <jhjh0022@naver.com>
2023-10-02 02:07:45 +0900

    FEAT : Tribe class

---
+++ Assignment/src/main/java/org/example/jaehyeon/tribe/Tribe.java
@@ -0,0 +1,27 @@
+package org.example.jaehyeon.tribe;
+
+
+import org.example.jaehyeon.starcraft.Unit;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import java.util.Random;
+
+public abstract class Tribe {
+
+    List<Unit> unitList;
+
+    public Unit randomProductionUnit(String[] unit) {
+        Random random = new Random();
+        try {
+            String selectUnit = unit[random.nextInt(unit.length)];
+
+            return (Unit) this.getClass().getMethod(selectUnit).invoke(selectUnit);
+
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
