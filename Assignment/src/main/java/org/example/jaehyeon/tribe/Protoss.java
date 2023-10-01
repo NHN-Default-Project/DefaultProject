be1a775891adcc463e585883ac6a78ba18ce4a18
hyeonjaez <jhjh0022@naver.com>
2023-10-02 02:54:37 +0900

    FEAT : Protoss class

---
+++ Assignment/src/main/java/org/example/jaehyeon/tribe/Protoss.java
@@ -0,0 +1,38 @@
+package org.example.jaehyeon.tribe;
+
+import org.example.jaehyeon.starcraft.protoss.*;
+
+import java.util.ArrayList;
+
+public class Protoss extends Tribe {
+
+    private static final String[] UNIT = {"zealot", "dragoon", "highTempler", "scout", "corsair"};
+    private static final int PRODUCTION_NUMBER = 4;
+
+    public Protoss() {
+        unitList = new ArrayList<>();
+        for (int i = 0; i < PRODUCTION_NUMBER; i++) {
+            unitList.add(randomProductionUnit(UNIT));
+        }
+    }
+
+    public Zealot zealot() {
+        return new Zealot();
+    }
+
+    public Dragoon dragoon() {
+        return new Dragoon();
+    }
+
+    public HighTempler highTempler() {
+        return new HighTempler();
+    }
+
+    public Scout scout() {
+        return new Scout();
+    }
+
+    public Corsair corsair() {
+        return new Corsair();
+    }
+}
