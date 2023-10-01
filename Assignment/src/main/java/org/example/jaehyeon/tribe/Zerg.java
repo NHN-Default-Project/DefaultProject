f6585f3acf466a20a3ab4e847425cdaa77b1d4a8
hyeonjaez <jhjh0022@naver.com>
2023-10-02 03:14:12 +0900

    REFACTOR: because of Tribe.java refactor

--- Assignment/src/main/java/org/example/jaehyeon/tribe/Zerg.java
+++ Assignment/src/main/java/org/example/jaehyeon/tribe/Zerg.java
@@ -1,2 +1,36 @@
-package org.example.jaehyeon.tribe;public class Zerg {
+package org.example.jaehyeon.tribe;
+
+import org.example.jaehyeon.starcraft.zerg.*;
+
+import java.util.ArrayList;
+
+public class Zerg extends Tribe {
+
+    private static final String[] UNIT = {"zergling", "hydralisk", "ultralisk", "mutalisk", "guardian"};
+    private static final int PRODUCTION_NUMBER = 8;
+
+    public Zerg() {
+        unitList = new ArrayList<>();
+        addUnit(PRODUCTION_NUMBER, UNIT);
+    }
+
+    public Zergling zergling() {
+        return new Zergling();
+    }
+
+    public Hydralisk hydralisk() {
+        return new Hydralisk();
+    }
+
+    public Ultralisk ultralisk() {
+        return new Ultralisk();
+    }
+
+    public Mutalisk mutalisk() {
+        return new Mutalisk();
+    }
+
+    public Guardian guardian() {
+        return new Guardian();
+    }
 }
