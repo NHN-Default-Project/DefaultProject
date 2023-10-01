8b24591dd654db551fbc369f2db7f48de88a8035
hyeonjaez <jhjh0022@naver.com>
2023-10-02 03:01:11 +0900

    FEAT: Zergling class

---
+++ Assignment/src/main/java/org/example/jaehyeon/starcraft/zerg/Zergling.java
@@ -0,0 +1,13 @@
+package org.example.jaehyeon.starcraft.zerg;
+
+import org.example.jaehyeon.starcraft.Unit;
+
+public class Zergling extends Unit {
+
+    private static final int EXTRA_ATTACK_POWER = 2;
+    private static final int EXTRA_DEFENSE_POWER = 2;
+
+    public Zergling() {
+        setAbility(EXTRA_ATTACK_POWER, EXTRA_DEFENSE_POWER);
+    }
+}
