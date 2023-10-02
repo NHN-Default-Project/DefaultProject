3db50b8e95c6144741974aa679b1a655b14c7978
hyeonjaez <jhjh0022@naver.com>
2023-10-02 18:17:28 +0900

    FEAT : because of Item, Flyable interface

--- Assignment/src/main/java/org/example/jaehyeon/starcraft/Unit.java
+++ Assignment/src/main/java/org/example/jaehyeon/starcraft/Unit.java
@@ -1,19 +1,69 @@
 package org.example.jaehyeon.starcraft;
 
-public abstract class Unit {
+public abstract class Unit implements Item, Flyable {
 
     int attackPower;
     int defensePower;
 
-    public void beAttacked(int attackPower){
-        this.defensePower -= attackPower;
+    boolean canFly;
+    boolean hasItem;
+
+    public void attackUnit(Unit targetUnit) {
+        if (!this.canFly) {
+            if (targetUnit.canFly && !this.hasItem) {
+                System.out.println("이 유닛은 공중에 있는 유닛을 공격을 못합니다.");
+                return;
+            }
+        }
+        attack(targetUnit);
     }
 
-    public void setAbility(int extraAttackPower, int extraDefensePower){
+
+
+    public void attack(Unit targetUnit) {
+        targetUnit.defensePower -= this.attackPower;
+        if (targetUnit.defensePower <= 0) {
+            System.out.println("파괴되었다");
+        } else {
+            System.out.println("공격했다");
+        }
+    }
+
+    public void beAttacked() {
+        this.defensePower -= attackPower;
+        if (this.defensePower <= 0) {
+            System.out.println("파괴되었다");
+        } else {
+            System.out.println("공격 당했다");
+        }
+    }
+
+    public void setAbility(int extraAttackPower, int extraDefensePower) {
         this.attackPower += extraAttackPower;
         this.defensePower += extraDefensePower;
     }
 
+    public boolean getHasItem() {
+        return this.hasItem;
+    }
+
+    protected void setCanFly() {
+        this.canFly = canFly();
+    }
+
+    protected void setItem() {
+        this.hasItem = hasItem();
+    }
+
+    @Override
+    public boolean canFly() {
+        return false;
+    }
+
+    @Override
+    public boolean hasItem() {
+        return false;
+    }
 
 
 }
