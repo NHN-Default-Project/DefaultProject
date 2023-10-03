fffcd1fd690950b820397e6d4b0ad6695c85be23
minsu <zmfjtnl1@naver.com>
2023-10-03 20:40:35 +0900

    Feat Chapter7 exercise7_2

--- Chapter7/src/main/java/com/nhnacademy/parkminsu/exercise7_2/Exercise7_2.java
+++ Chapter7/src/main/java/com/nhnacademy/parkminsu/exercise7_2/Exercise7_2.java
@@ -4,20 +4,30 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Exercise7_2 {
+    /* 전치 행렬을 구하는 문제
+     * T[i][j] = M[j][i]
+     * 전치를 반환하는 함수를 작성
+     * */
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.println("row, col을 입력하세요");
             int row = scanner.nextInt();
             int col = scanner.nextInt();
-            if (row <= 0 || col <= 0) {
-                throw new IllegalArgumentException("0이하의 숫자를 입력하셨습니다.");
-            }
+            preconditionRowNumAndColNum(row, col);
             int[][] matrix = createMatrixValue(row, col);
             int[][] transposeMatrix = transpose(row, col, matrix);
             printMatrix(matrix);
             printMatrix(transposeMatrix);
         } catch (InputMismatchException e) {
             throw new InputMismatchException("잘못 입력하셨습니다.");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void preconditionRowNumAndColNum(int row, int col) {
+        if (row <= 0 || col <= 0) {
+            throw new IllegalArgumentException("0이하의 숫자를 입력하셨습니다.");
         }
     }
 
