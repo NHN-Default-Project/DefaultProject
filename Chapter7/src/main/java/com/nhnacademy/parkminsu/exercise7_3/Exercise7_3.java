d08911a3f3eb0d79e318a71033f24daef0f86601
minsu <zmfjtnl1@naver.com>
2023-10-04 16:18:42 +0900

    Feat Chapter7 exercise7_3

--- Chapter7/src/main/java/com/nhnacademy/parkminsu/exercise7_3/Exercise7_3.java
+++ Chapter7/src/main/java/com/nhnacademy/parkminsu/exercise7_3/Exercise7_3.java
@@ -22,32 +22,26 @@ public class Exercise7_3 {
         selectionSortArray = selectionSort(selectionSortArray);
         endTime = System.nanoTime();
         System.out.println("선택정렬 시간: " + ((endTime - startTime) / 1000000000.0));
-        //printArray(selectionSortArray);
 
         startTime = System.nanoTime();
         Arrays.sort(sortArray);
         endTime = System.nanoTime();
         System.out.println("퀵 정렬 시간: " + ((endTime - startTime) / 1000000000.0));
-        //printArray(sortArray);
 
         startTime = System.nanoTime();
         selectionSortStringArray = selectionSortStringArray(selectionSortStringArray);
         endTime = System.nanoTime();
         System.out.println("선택정렬 시간: " + ((endTime - startTime) / 1000000000.0));
-        //printArray(selectionSortStringArray);
 
         startTime = System.nanoTime();
         Arrays.sort(sortStringArray);
         endTime = System.nanoTime();
         System.out.println("퀵 정렬 시간: " + ((endTime - startTime) / 1000000000.0));
-        //printArray(sortArray);
-
-
     }
 
     public static double[] createArrayValue(double[] array, int size) {
         for (int i = 0; i < size; i++) {
-            array[i] = Math.random() * ARRAYSIZE;
+            array[i] = Math.random() * size;
         }
         return array;
     }
@@ -56,16 +50,15 @@ public class Exercise7_3 {
         double min;
         int tmpIndex = 0;
 
-        for (int i = 0; i < ARRAYSIZE; i++) {
-            double tmpNum = Double.MAX_VALUE;
+        for (int i = 0; i < selectionSortArray.length; i++) {
             min = selectionSortArray[i];
-            for (int j = i; j < ARRAYSIZE; j++) {
+            for (int j = i; j < selectionSortArray.length; j++) {
                 if (min > selectionSortArray[j]) {
                     min = selectionSortArray[j];
                     tmpIndex = j;
                 }
             }
-            tmpNum = selectionSortArray[tmpIndex];
+            double tmpNum = selectionSortArray[tmpIndex];
 
             selectionSortArray[tmpIndex] = selectionSortArray[i];
             selectionSortArray[i] = tmpNum;
@@ -74,13 +67,13 @@ public class Exercise7_3 {
     }
 
     public static void printArray(double... array) {
-        for (int i = 0; i < ARRAYSIZE; i++) {
+        for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
         }
     }
 
     public static void printArray(String... array) {
-        for (int i = 0; i < ARRAYSIZE; i++) {
+        for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
         }
     }
@@ -90,13 +83,11 @@ public class Exercise7_3 {
         int rightLimit = 122;
         int randomStringLength = 10;
         Random random = new Random();
-        for (int i = 0; i < ARRAYSIZE; i++) {
-            String randomString =
-                    strings[i] = random.ints(leftLimit, rightLimit + 1)
-                            .limit(randomStringLength)
-                            .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-                            .toString();
-            strings[i] = randomString;
+        for (int i = 0; i < strings.length; i++) {
+            strings[i] = random.ints(leftLimit, rightLimit + 1)
+                    .limit(randomStringLength)
+                    .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                    .toString(); // 랜덤한 문자열 생성
         }
         return strings;
     }
