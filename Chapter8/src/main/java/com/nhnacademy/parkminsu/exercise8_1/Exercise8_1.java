0bc0bda6b0bfec3ae5b6807afbb4d08295a88559
minsu <zmfjtnl1@naver.com>
2023-10-05 09:58:23 +0900

    Feat Chapter8 exercise8_1

--- Chapter8/src/main/java/com/nhnacademy/parkminsu/exercise8_1/Exercise8_1.java
+++ Chapter8/src/main/java/com/nhnacademy/parkminsu/exercise8_1/Exercise8_1.java
@@ -7,43 +7,43 @@ public class Exercise8_1 {
     // yes 또는 no라는 대답이 들어왔을 때 예외처리하기
     // root라는 메소드를 이차 방정식 객체에 넣어도 되는지 고민
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            String userAnswer = "";
-            QuadraticEquation quadraticEquation = new QuadraticEquation();
-            while (!userAnswer.equals("no")) { // loop문 다듬기
+        Scanner scanner = new Scanner(System.in);
+        String userAnswer = "";
+        QuadraticEquation quadraticEquation = new QuadraticEquation();
+        while (!userAnswer.equals("no")) { // loop문 다듬기
+            try {
                 System.out.println("계수를 입력하세요(이차항 계수 순서부터)");
-                quadraticEquation.enterCoefficient(scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
-                System.out.println(root(quadraticEquation.getQuadraticCoefficient(), quadraticEquation.getLinearTermCoefficient(), quadraticEquation.getConstant()));
+                quadraticEquation.enterCoefficient(scanner.nextDouble(), scanner.nextDouble(), scanner.nextDouble());
+                System.out.println(quadraticEquation.root());
+                userAnswer = inputUserAnswer(scanner);
+            } catch (InputMismatchException e) {
+                System.out.println("정수 또는 실수 아닌 값을 입력했습니다. 다시 입력해주세요");
+            }
+        }
+        System.out.println("프로그램 종료");
+        scanner.close();
+    }
+
+    public static String inputUserAnswer(Scanner scanner) {
+        String userAnswer = "";
+        while (true) {
+            try {
                 System.out.println("다른 방정식을 입력하시겠습니까?(yes or no)");
-                userAnswer = scanner.next().toLowerCase();
-                if (!userAnswer.equals("yes") || !userAnswer.equals("no")) {
-                    throw new IllegalArgumentException("답변을 잘못했습니다.");
-                }
-            }
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        } catch (InputMismatchException e) {
-            System.out.println("개수를 잘못 입력하셨습니다.");
-        }
+                userAnswer = scanner.next().toLowerCase().trim();
+                checkUserCorrectAnswer(userAnswer);
+                return userAnswer;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
 
-    }
-
-    /**
-     * A*x*x + B*x + C = 0 형태의 이차방정식의 해 중에서 큰 해를 반환합니다.
-     * 해가 있는 경우에만 작동하며, A == 0 이거나 판별식인 B*B - 4*A*C 가 음수인 경우
-     * IllegalArgumentException 예외가 발생합니다.
-     */
-    public static double root(double A, double B, double C)
-            throws IllegalArgumentException {
-        if (A == 0) {
-            throw new IllegalArgumentException("A는 0일 수 없습니다.");
-        } else {
-            double disc = B * B - 4 * A * C;
-            if (disc < 0) {
-                throw new IllegalArgumentException("판별식 < 0.");
             }
-            return (-B + Math.sqrt(disc)) / (2 * A);
         }
     }
+
+    public static void checkUserCorrectAnswer(String userAnswer) {
+        if (!userAnswer.equals("yes") && !userAnswer.equals("no")) {
+            throw new IllegalArgumentException("답변을 잘못했습니다.");
+        }
+    }
+
 
 }
--- Chapter8/src/main/java/com/nhnacademy/parkminsu/exercise8_1/QuadraticEquation.java
+++ Chapter8/src/main/java/com/nhnacademy/parkminsu/exercise8_1/QuadraticEquation.java
@@ -1,9 +1,9 @@
 package com.nhnacademy.parkminsu.exercise8_1;
 
-public class QuadraticEquation {
-    private int quadraticCoefficient;
-    private int linearTermCoefficient;
-    private int constant;
+public class QuadraticEquation { // 2차 방정식 클래스
+    private double quadraticCoefficient;
+    private double linearTermCoefficient;
+    private double constant;
 
     public QuadraticEquation() {
         this.quadraticCoefficient = 0;
@@ -11,21 +11,40 @@ public class QuadraticEquation {
         this.constant = 0;
     }
 
-    public void enterCoefficient(int quadraticCoefficient, int linearTermCoefficient, int constant) {
+    public void enterCoefficient(double quadraticCoefficient, double linearTermCoefficient, double constant) {
         this.quadraticCoefficient = quadraticCoefficient;
         this.linearTermCoefficient = linearTermCoefficient;
         this.constant = constant;
     }
 
-    public int getQuadraticCoefficient() {
+
+    /**
+     * A*x*x + B*x + C = 0 형태의 이차방정식의 해 중에서 큰 해를 반환합니다.
+     * 해가 있는 경우에만 작동하며, A == 0 이거나 판별식인 B*B - 4*A*C 가 음수인 경우
+     * IllegalArgumentException 예외가 발생합니다.
+     */
+    public double root()
+            throws IllegalArgumentException {
+        if (this.quadraticCoefficient == 0) {
+            throw new IllegalArgumentException("A는 0일 수 없습니다.");
+        } else {
+            double disc = this.linearTermCoefficient * this.linearTermCoefficient - 4 * this.quadraticCoefficient * this.constant;
+            if (disc < 0) {
+                throw new IllegalArgumentException("판별식 < 0.");
+            }
+            return (-this.linearTermCoefficient + Math.sqrt(disc)) / (2 * this.quadraticCoefficient);
+        }
+    }
+
+    public double getQuadraticCoefficient() {
         return quadraticCoefficient;
     }
 
-    public int getLinearTermCoefficient() {
+    public double getLinearTermCoefficient() {
         return linearTermCoefficient;
     }
 
-    public int getConstant() {
+    public double getConstant() {
         return constant;
     }
 }
