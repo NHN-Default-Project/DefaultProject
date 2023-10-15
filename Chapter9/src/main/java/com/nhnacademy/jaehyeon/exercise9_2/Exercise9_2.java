ce87415993f9b5af35fe4bb5400b2efa6397e873
hyeonjaez <jhjh0022@naver.com>
2023-10-15 14:51:13 +0900

    "modify: Chapter 9 : Problem Description"

--- Chapter9/src/main/java/com/nhnacademy/jaehyeon/exercise9_2/Exercise9_2.java
+++ Chapter9/src/main/java/com/nhnacademy/jaehyeon/exercise9_2/Exercise9_2.java
@@ -3,6 +3,10 @@ package com.nhnacademy.jaehyeon.exercise9_2;
 import java.io.*;
 import java.util.Arrays;
 
+/*
+* https://math.hws.edu/javanotes/c9/exercises.html
+* txt 파일의 모든 단어를 알파벳 순으로 나열하고 이 목록을 다른 파일에 저장하는 프로그램을 이진 정렬트리에 단어를 저장하도록 하자.
+ */
 public class Exercise9_2 {
     public static void main(String[] args) {
         String inputFilePath = "/Users/hyeon/Desktop/DefaultProject/DefaultProject/Chapter9/src/main/java/com/nhnacademy/jaehyeon/exercise9_2/read.txt";
--- Chapter9/src/main/java/com/nhnacademy/jaehyeon/exercise9_3/Exercise9_3.java
+++ Chapter9/src/main/java/com/nhnacademy/jaehyeon/exercise9_3/Exercise9_3.java
@@ -1,6 +1,9 @@
 package com.nhnacademy.jaehyeon.exercise9_3;
 
-
+/*
+* https://math.hws.edu/javanotes/c9/exercises.html
+* 리스트 항목의 순서를 반대로 한 복사본을 생성하자 이 서브루틴은 ListNode 유형의 매개변수를 가져야 하며, ListNode 유형의 값을 반환 해야한다.
+ */
 public class Exercise9_3 {
 
     public static void main(String[] args) {
