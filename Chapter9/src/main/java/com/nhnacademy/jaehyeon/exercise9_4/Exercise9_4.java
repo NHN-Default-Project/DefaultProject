85f37d178cbe8be97a6d57605b75836e24a96bae
hyeonjaez <jhjh0022@naver.com>
2023-10-15 14:21:06 +0900

    modify:Exercise9_4

--- Chapter9/src/main/java/com/nhnacademy/jaehyeon/exercise9_4/Exercise9_4.java
+++ Chapter9/src/main/java/com/nhnacademy/jaehyeon/exercise9_4/Exercise9_4.java
@@ -3,24 +3,19 @@ package com.nhnacademy.jaehyeon.exercise9_4;
 public class Exercise9_4 {
     public static void main(String[] args) {
 
-        TreeNode root = new TreeNode(1);
-//        root.left = new TreeNode(2);
-//        root.right = new TreeNode(3);
-//        root.left.left = new TreeNode(4);
-//        root.left.right = new TreeNode(5);
-//        root.right.left = new TreeNode(6);
-//        root.right.right = new TreeNode(7);
-//        root.left.left.left = new TreeNode(8);
-//        root.left.left.right = new TreeNode(9);
-//        root.left.right.left = new TreeNode(10);
-//        root.left.right.right = new TreeNode(11);
-//        root.right.left.left = new TreeNode(12);
-//        root.right.left.right = new TreeNode(13);
-//        root.right.right.left = new TreeNode(14);
-//        root.right.right.right = new TreeNode(15);
+        TreeNode node = new TreeNode();
 
-        TreeNodeQueue treeNodeQueue = new TreeNodeQueue();
-        treeNodeQueue.printNode(root);
+        node.insert(3);
+        node.insert(4);
+        node.insert(1);
+        node.insert(2);
+        node.insert(5);
+        node.insert(6);
+        node.insert(7);
+        node.insert(8);
+
+        node.printNode();
+
 
     }
 }
--- Chapter9/src/main/java/com/nhnacademy/jaehyeon/exercise9_4/TreeNode.java
+++ Chapter9/src/main/java/com/nhnacademy/jaehyeon/exercise9_4/TreeNode.java
@@ -1,13 +1,59 @@
 package com.nhnacademy.jaehyeon.exercise9_4;
 
-public class TreeNode {
-    int value;
-    TreeNode left;
-    TreeNode right;
+import java.util.LinkedList;
+import java.util.Queue;
 
-    public TreeNode(int value) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
+public class TreeNode {
+
+    private Node root;
+
+    public void insert(int value) {
+        this.root = insertNode(this.root, value);
     }
+
+    public Node insertNode(Node root, int value) {
+        if (root == null) {
+            root = new Node(value);
+            return root;
+        }
+
+        if (root.value > value) {
+            root.left = insertNode(root.left, value);
+        } else if (root.value < value) {
+            root.right = insertNode(root.right, value);
+        }
+        return root;
+    }
+
+    public void printNode() {
+        Queue<Node> queue = new LinkedList<>();
+        Node current = root;
+
+        while (current != null || !queue.isEmpty()) {
+            while (current != null) {
+                queue.add(current);
+                current = current.left;
+            }
+
+            current = queue.poll();
+            System.out.println(current.value);
+
+            current = current.right;
+        }
+    }
+
+
+    public static class Node {
+        int value;
+        Node left;
+        Node right;
+
+        public Node(int value) {
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+
 }
