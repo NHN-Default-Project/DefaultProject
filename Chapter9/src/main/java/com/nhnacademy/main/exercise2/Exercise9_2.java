f71184d85c170e3e68cdff8960643e2cce606d61
nannda3463 <minsu3463@gmail.com>
2023-10-16 09:40:05 +0900

    RENAME (Chapter9) : 문제 설명 추가

--- Chapter9/src/main/java/com/nhnacademy/main/exercise2/Exercise9_2.java
+++ Chapter9/src/main/java/com/nhnacademy/main/exercise2/Exercise9_2.java
@@ -1,5 +1,11 @@
 package com.nhnacademy.main.exercise2;
 
+/**
+ * 1. txt 파일을 읽기
+ * 2. 읽은 문자열을 소문자로 변환하기
+ * 3. 변환한 문자열을 BinaryTree에 저장하기
+ * 4. 저장한 Tree의 값들을 꺼내어 txt 파일로 저장하기
+ */
 public class Exercise9_2 {
     public static void main(String[] args) {
         ReadAndWriteFileByTree textFile = new ReadAndWriteFileByTree();
