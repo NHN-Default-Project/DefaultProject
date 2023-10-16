c706ccdd1b0b67336eb02af876563fc2ef8c4bb1
hyeonjaez <jhjh0022@naver.com>
2023-10-16 10:06:08 +0900

    feat: Exercise9_3 문제설명

--- Chapter9/src/main/java/com/nhnacademy/jaehyeon/main/exercise9_3/Exercise9_3.java
+++ Chapter9/src/main/java/com/nhnacademy/jaehyeon/main/exercise9_3/Exercise9_3.java
@@ -3,6 +3,10 @@ package com.nhnacademy.jaehyeon.main.exercise9_3;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/*
+ * https://math.hws.edu/javanotes/c9/exercises.html
+ * 리스트 항목의 순서를 반대로 한 복사본을 생성하자 이 서브루틴은 ListNode 유형의 매개변수를 가져야 하며, ListNode 유형의 값을 반환 해야한다.
+ */
 public class Exercise9_3 {
     public static void main(String[] args) {
         CustomLinkedList<Integer> list;
