5cb2123437ccb63fa5ed7e05ae79b7b3acf7a4e1
nannda3463 <minsu3463@gmail.com>
2023-11-06 10:24:03 +0900

    FEAT (Thread) Thread 연습문제, 과제 푸시

---
+++ Thread/src/main/java/com/nhnacademy/Jminsoo/one/Main.java
@@ -0,0 +1,21 @@
+package com.nhnacademy.Jminsoo.one;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Mart mart = new Mart();
+        Producer producer = new Producer(mart);
+        producer.start();
+
+
+        int index = 0;
+        while (true) {
+            index++;
+            new Consumer("손님" + index, mart).start();
+            int randNum = ThreadLocalRandom.current().nextInt(1, 4);
+            Thread.sleep(randNum * 1_000L);
+        }
+    }
+}
